bc30c18fc3a567ca77bf81bc81cbb13073848883
MukeshKuBehera <mukeshkubehera1@gmail.com>
2022-09-06 17:47:21 +0530

    spring-security-jdbc

---
+++ src/main/java/com/sec/Project23SpringSecurityJdbc2Application.java
@@ -0,0 +1,13 @@
+package com.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project23SpringSecurityJdbc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project23SpringSecurityJdbc2Application.class, args);
+	}
+
+}
